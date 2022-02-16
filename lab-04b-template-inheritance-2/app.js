41f783cb5142320ea0684bbfe5049ca5a02871e0
neomq <neominqi@hotmail.com>
2022-02-16 17:51:27 +0000

    done lab-04b

---
+++ lab-04b-template-inheritance-2/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const wax = require('wax-on');
+const hbs = require('hbs');
+var helpers = require('handlebars-helpers')({
+  handlebars: hbs.handlebars
+});
+const path = require('path');
+let app = express();
+
+app.set('view engine', hbs);
+app.set('views', path.join(__dirname, '/views'));
+
+wax.on(hbs.handlebars);
+wax.setLayoutPath(path.join(__dirname, '/views/layouts'))
+
+// YOUR CODE HERE
+app.get('/', function(req,res){
+  res.render('welcome')
+})
+
+
+app.get('/', (req,res)=>{
+    res.render('form.hbs');
+})
+module.exports = app;
